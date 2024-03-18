7a34e5bfbd50830024605e0ec4d0dd5d467bff5b
beatrizafa <163016791+beatrizafa@users.noreply.github.com>
2024-03-18 18:54:33 +0100

    search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+  //console.log(apiUrl)//
+  let temperatureElement = document.querySelector("#current-temperature-value");
+  let currentTemperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(currentTemperature);
+}
+
+function searchCity(city) {
+  // make api call and update the interface//
+  let apiKey = "a017cf6758e1482b564bdt6545doc30a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  // move this to response to correct the name
+  //let cityElement = document.querySelector("#city");//
+  //cityElement.innerHTML = searchInput.value;//
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
 
-let apiUrl = "a017cf6758e1482b564bdt6545doc30a";
+searchCity("Barcelona");
